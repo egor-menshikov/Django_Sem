e207ec7516eb6741e797d1cee6816e7af64e7b93
Egor Menshikov <r3born.fs@gmail.com>
2024-04-16 14:47:26 +0300

    created an app with index view, connected to the project

---
+++ seminar_project/sem_1_1_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Sem11AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sem_1_1_app'
--- seminar_project/seminar_project/settings.py
+++ seminar_project/seminar_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sem_1_1_app',
 ]
 
 MIDDLEWARE = [
