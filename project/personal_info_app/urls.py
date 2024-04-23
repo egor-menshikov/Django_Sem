a0986892057590f0f3c7aef35990668f6ca145d4
Egor Menshikov <r3born.fs@gmail.com>
2024-04-23 12:46:45 +0300

    sem_3 task_1 done

--- project/personal_info_app/urls.py
+++ project/personal_info_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AboutTemplate
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    # path('about/', about, name='about'),
+    path('about/', AboutTemplate.as_view(), name='about'),
 ]
--- project/personal_info_app/views.py
+++ project/personal_info_app/views.py
@@ -6,23 +6,16 @@ from django.views.generic import TemplateView
 
 logger = logging.getLogger(__name__)
 
-_ABOUT = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>About me</title>
-</head>
-<body>
-<p>Меня зовут Егор, я учусь на веб-разработчика.</p>
-</body>
-</html>
-"""
-
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'personal_info_app')
+        return render(request, 'personal_info_app/index.html')
 
 
 class AboutTemplate(TemplateView):
-    pass
+    template_name = 'personal_info_app/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Егор'
+        return context
